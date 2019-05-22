cddb7b65b662f352ee8990e17c14d7f470f1b8b6
Juan <jdiaz@optimeconsulting.com>
2019-05-22 14:13:29 -0500

    validacion de correo xd js

---
+++ Factory-Industrial-Template/contactform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JDIAZ
+ * Date: 22/05/2019
+ * Time: 1:38 PM
+ */
+class Send {
+
+    public function sendEmail (){
+        echo "enviado";
+    }
+
+}
